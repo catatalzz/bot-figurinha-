0ba469d19ca23e454af47afce44d8b8564fdd97a
catatalzz <148815684+catatalzz@users.noreply.github.com>
2026-09-24 19:40:49 -0300

    Add configuration file for bot settings

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    nomeBot: "nao sei",
+    prefixo: "/",
+
+    // Coloque seu número aqui com DDI.
+    // Exemplo: 5511999999999
+    dono: "5511999999999",
+
+    maxVideoSeconds: 10,
+    maxImageSizeMB: 10
+};
